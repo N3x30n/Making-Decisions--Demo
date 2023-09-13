7a4cbe69e2f9bf499637daa13e87f14a38e8cb44
Atlas Tran <atlas.tran123@gmail.com>
2023-09-13 14:29:51 -0600

    added Jamie Lannister second attack

--- varsAndConditionals.js
+++ varsAndConditionals.js
@@ -18,9 +18,24 @@ if (jonSnowAttack > jamieLannisterAttack){
 let jonSnowHealth = 100;
 let jonSnowDefense = 0;
 
+console.log('Jamie Lannister attacks Jon Snow')
+
 if (jonSnowHealth <= jamieLannisterAttack){
     console.log('Jon Snow has been slain')
 } else {
     jonSnowHealth = jonSnowHealth - jamieLannisterAttack
     console.log(`Jon Snow's Health is down to ${jonSnowHealth}`)
+}
+
+jonSnowDefense += 25;
+
+console.log('Jon Snow has picked up a shield that increases his defense by 25')
+//second attack from Jamie
+console.log('Jamie Lannister attacks Jon Snow again')
+
+if (jonSnowHealth <= jamieLannisterAttack - jonSnowDefense){
+    console.log('Jon Snow has been slain')
+} else {
+    jonSnowHealth -= (jamieLannisterAttack-jonSnowDefense)
+    console.log(`Jon Snow's health is down to ${jonSnowHealth}`)
 }
